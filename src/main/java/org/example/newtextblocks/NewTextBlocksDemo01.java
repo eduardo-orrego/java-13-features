9f15765f3a2be38ab18f5457e81eeabf32b48b1a
Eduardo Orrego <eduardo.orrego@outlook.com>
2024-05-07 00:41:11 -0500

    Java 13 Features - JEP 355: Text Blocks (Preview)

---
+++ src/main/java/org/example/newtextblocks/NewTextBlocksDemo01.java
@@ -0,0 +1,24 @@
+package org.example.newtextblocks;
+
+public class NewTextBlocksDemo01 {
+    public static void main(String[] args) {
+        System.out.println("-----Using 'one-dimensional' string literals-----");
+
+        String html = "<html>\n" +
+                "    <body>\n" +
+                "        <p>Hello, world</p>\n" +
+                "    </body>\n" +
+                "</html>\n";
+        System.out.println(html);
+
+        System.out.println("-----Using a 'two-dimensional' block of text-----");
+        String html2 = """
+              <html>
+                  <body>
+                      <p>Hello, world</p>
+                  </body>
+              </html>
+              """;
+        System.out.println(html2);
+    }
+}
